646444c29b3844001d93f2c89eecd78bd1de939f
Chris Underwood <cunderwoodmn@gmail.com>
2017-12-23 17:03:21 -0800

    app can now draw with one color and no longer have the line start at 0,0 or start a new line at the end of old one.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.stokeStyle = '#badA55';
+ctx.strokeStyle = '#badA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
@@ -15,9 +15,19 @@ let lastY = 0;
 function draw(e){
   if(!isDrawing){ return; }
   console.log(e);
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  lastX = e.offsetX;
+  lastY = e.offsetY;
 }
 
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+});
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
